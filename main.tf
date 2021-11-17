fd50d0e43df2f100c9511ed9ab248af7f3764176
marzojl <marzojl@gmail.com>
2021-11-16 19:11:27 -0800

    updated main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.region
+}
+
+module "s3-webapp" {
+  source  = "app.terraform.io/marzojl-terra/s3-webapp/aws"
+  name        = var.name
+  region = var.region
+  prefix = var.prefix
+  version = "1.0.0"
+  
+}
+
